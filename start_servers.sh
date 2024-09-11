24d34d083ad4924c12b35eddc4ed7ee6f69b2967
Harrison <harrison.vanderbyl@gmail.com>
2024-09-11 19:56:20 +1000

    startup script

--- start_servers.sh
+++ start_servers.sh
@@ -1,3 +1,18 @@
-apt install tmux
+apt update && apt install tmux -y
+
+git clone https://github.com/myshell-ai/MeloTTS.git
+cd MeloTTS
+pip install -e .
+python -m unidic download
+
+cd ../
+
 cd voice_servers 
+
+cd models/cottontail-lg && wget https://huggingface.co/Hazzzardous/cottontail/resolve/main/expxl.pt
+cd ../..
+
+cd models/cottontail-sm && wget https://huggingface.co/Hazzzardous/cottontail/resolve/main/exp.pt
+cd ../..
+
 pip install -r ./requirements.txt && python3 ./server_hub.py
